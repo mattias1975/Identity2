8797b6e26f11961dc83b97a1feb263bd1a7b3a6a
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-10-16 13:26:52 +0200

    1326

--- Models/IdentityViewModels.cs
+++ Models/IdentityViewModels.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Identity.Migrations;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -11,6 +12,8 @@ namespace Identity.Models
         [Required]
         [Display(Name ="Username")]
         public string UserName { get; set; }
+
+        
 
         [Required]
         [DataType(DataType.Password)]
